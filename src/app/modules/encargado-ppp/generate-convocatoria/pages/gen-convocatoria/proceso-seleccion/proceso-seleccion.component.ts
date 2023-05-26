f1ff7df25c0977d183e3a488c12e169490e980d8
renatzord <renato.llivisaca.est@tecazuay.edu.ec>
2023-05-26 01:02:54 -0500

    carpetas creadas

---
+++ src/app/modules/encargado-ppp/generate-convocatoria/pages/gen-convocatoria/proceso-seleccion/proceso-seleccion.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-proceso-seleccion',
+  templateUrl: './proceso-seleccion.component.html',
+  styleUrls: ['./proceso-seleccion.component.css']
+})
+export class ProcesoSeleccionComponent {
+
+}
