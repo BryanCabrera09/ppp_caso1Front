cdd3b4f7fbd5d222e7a0d3d2b7e5be79b3d0f4d7
Andres Zuin <jonathan.zuin.est@tecazuay.edu.ec>
2023-05-26 19:18:35 -0500

    Plan de aprendizaje

--- src/app/modules/tutor-academico/plan-aprendizaje/plan-aprendizaje.module.ts
+++ src/app/modules/tutor-academico/plan-aprendizaje/plan-aprendizaje.module.ts
@@ -4,6 +4,7 @@ import { CommonModule } from '@angular/common';
 import { PlanAprendizajeRoutingModule } from './plan-aprendizaje-routing.module';
 import { GenerarPlanComponent } from './generar-plan/generar-plan.component';
 import { PracticasTutorComponent } from './practicas-tutor/practicas-tutor.component';
+import { FormsModule } from '@angular/forms';
 
 
 @NgModule({
@@ -13,7 +14,8 @@ import { PracticasTutorComponent } from './practicas-tutor/practicas-tutor.compo
   ],
   imports: [
     CommonModule,
-    PlanAprendizajeRoutingModule
+    PlanAprendizajeRoutingModule,
+    FormsModule
   ]
 })
 export class PlanAprendizajeModule { }
