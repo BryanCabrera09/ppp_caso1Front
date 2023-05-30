8ff8e01c293e0fc410e4bf745ed0053c1cd3ec63
Jaqueline11 <miriancutiopala123@gmail.com>
2023-05-30 14:59:01 -0500

    generar pdf

--- src/app/modules/practicante/anexos/pages/listprac/listprac.component.ts
+++ src/app/modules/practicante/anexos/pages/listprac/listprac.component.ts
@@ -12,6 +12,8 @@ import { SemanaActividadService } from 'src/app/core/services/semana-actividad.s
 import { TutorAcademicoService } from 'src/app/core/services/tutor-academico.service';
 import { NgxFileDropEntry, FileSystemFileEntry, FileSystemDirectoryEntry } from 'ngx-file-drop';
 import { ToastrService } from 'ngx-toastr';
+import * as pdfMake from 'pdfmake/build/pdfmake';
+import * as pdfFonts from 'pdfmake/build/vfs_fonts';
 
 
 
@@ -127,6 +129,7 @@ export class ListpracComponent implements OnInit{
 
     this.practica.concluciones=this.conclusion;
     this.editarPractica(this.practica.id, this.practica);
+    this.generarPDF1();
   }
 
   obtenerActividades(id: number) {
@@ -154,6 +157,199 @@ export class ListpracComponent implements OnInit{
       };
       reader.readAsDataURL(blob);
     });
+  }
+  async generarPDF1() {
+
+    const nomnreEst = this.usuario.nombre.split(' ').map(word => word.charAt(0).toUpperCase() + word.slice(1).toLowerCase()).join(' ') + ' ' + this.usuario.apellido.split(' ').map(word => word.charAt(0).toUpperCase() + word.slice(1).toLowerCase()).join(' ')
+    const nombreInstituto = this.practica.tutorInstituto.usuario.nombre.split(' ').map(word => word.charAt(0).toUpperCase() + word.slice(1).toLowerCase()).join(' ') + ' ' + this.practica.tutorInstituto.usuario.apellido.split(' ').map(word => word.charAt(0).toUpperCase() + word.slice(1).toLowerCase()).join(' ');
+    const nombreEspecifico = this.practica.tutorEmpresarial.usuario.nombre.split(' ').map(word => word.charAt(0).toUpperCase() + word.slice(1).toLowerCase()).join(' ') + ' ' + this.practica.tutorEmpresarial.usuario.apellido.split(' ').map(word => word.charAt(0).toUpperCase() + word.slice(1).toLowerCase()).join(' ');
+    const fecha = new Date();
+    const options: any = {
+      year: 'numeric',
+      month: 'long',
+      day: 'numeric'
+    };
+    const formatter = new Intl.DateTimeFormat('es-EC', options);
+    const fechaFormateada = formatter.format(fecha);
+    const fechaCompleta = `Cuenca, ${fechaFormateada}`;
+
+    function convertirNumeroEnLetras(numero) {
+      const numerosEnLetras = ['Cero', 'Primero', 'Segundo', 'Tercero', 'Cuarto', 'Quinto', 'Egresado']; // Agrega más elementos según necesites
+      return numerosEnLetras[numero] || '';
+    }
+    
+
+    // Logo Ista 
+    const imageData = await this.getBase64ImageFromAssets("assets/images/Logo-ISTA.png");
+
+    pdfMake.vfs = pdfFonts.pdfMake.vfs;
+
+    // Definir las márgenes del documento
+    var marginLeft = 74;
+    var marginRight = 74;
+    var marginTop = 70;
+    var marginBottom = 100;
+
+    const documentDefinition = {
+      pageSize: 'A4',
+      pageMargins: [marginLeft, marginTop, marginRight, marginBottom],
+      header: function (currentPage, pageCount) {
+        return {
+          image: imageData,
+          width: 145,
+          height: 45,
+          alignment: "left",
+          margin: [50, 15, -10, 100],
+        };
+      },
+      content: [
+
+        {
+          text: 'INSTITUTO SUPERIOR UNIVERSITARIO TECNOLÓGICO DEL AZUAY \nTECNOLOGÍA SUPERIOR EN DESARROLLO DE SOFTWARE \nANEXO 8: INFORME FINAL DE ACTIVIDADES DE PRÁCTICAS PRE \nPROFESIONALES',
+          style: 'header',
+          alignment: "center",
+          lineHeight: 1.5,
+          marginLeft: -12
+        },
+        {
+          text: fechaCompleta,
+          style: 'subheader',
+          alignment: 'right',
+          margin: [0, -5, 0, 0],
+          lineHeight: 1.5
+        },
+        {
+          text: '1. INFORMACIÓN GENERAL',
+          style: 'subheaderBold',
+          margin: [0, 10, 0, 0]
+        },
+        {
+          style: 'table',
+          table: {
+            widths: ['auto', '*', '*', '*'],
+            body: [
+              [{ text: 'A. DATOS GENERALES DE LA EMPRESA / INSTITUCIÓN', colSpan: 4, style: 'tableHeader' }, '', '', ''],
+              ['NOMBRE:', { text: this.practica.convocatoria.solicitudEmpresa.convenio.empresa.nombre, colSpan: 3, style: 'tableHeader' }, '', ''],
+              ['UBICACIÓN:\n(Ciudad y Dirección)', { text: 'Avenida General Torres, Edifcio Impacto', colSpan: 3, style: 'tableHeader' }, '', ''],
+              ['ÁREA O DEPARTAMENTO', { text: 'Departamento', colSpan: 3, style: 'tableHeader' }, '', ''],
+              [{ text: 'B. DATOS DEL TUTOR ESPECÍFICO', colSpan: 4, style: 'tableHeader' }, '', '', ''],
+              ['NOMBRE COMPLETO:', nombreEspecifico, 'CÉDULA DE \nIDENTIDAD:', this.practica.tutorEmpresarial.usuario.cedula],
+              ['CARGO:', this.practica.tutorEmpresarial.cargo, 'TELÉFONO EMPRESA:', '22054561625'],
+              ['CORREO ELECTRÓNICO:', { text: this.practica.tutorEmpresarial.usuario.correo, colSpan: 3, style: 'tableHeader' }, '', ''],
+              [{ text: 'C. DATOS DEL ESTUDIANTE', colSpan: 4, style: 'tableHeader' }, '', '', ''],
+              ['NOMBRE COMPLETO:', { text: nomnreEst, colSpan: 3, style: 'tableHeader' }, '', ''],
+              ['CÉDULA DE IDENTIDAD:', { text: this.practica.estudiante.usuario.cedula, colSpan: 3, style: 'tableHeader' }, '', ''],
+              ['CICLO:', { text: convertirNumeroEnLetras(this.practica.estudiante.ciclo), colSpan: 3, style: 'tableHeader' }, '', ''],
+              ['CORREO ELECTRÓNICO:', { text: this.practica.estudiante.usuario.correo, colSpan: 3, style: 'tableHeader' }, '', ''],
+              ['TELÉFONO:', { text: this.practica.estudiante.usuario.telefono, colSpan: 3, style: 'tableHeader' }, '', ''],
+              [{ text: 'D. DATOS DEL TUTOR ACADÉMICO', colSpan: 4, style: 'tableHeader' }, '', '', ''],
+              ['NOMBRE COMPLETO:', { text: nombreInstituto, colSpan: 3, style: 'tableHeader' }, '', ''],
+              ['CÉDULA DE IDENTIDAD:', { text: this.practica.tutorInstituto.usuario.cedula, colSpan: 3, style: 'tableHeader' }, '', ''],
+              ['CORREO ELECTRÓNICO:', { text: this.practica.tutorInstituto.usuario.correo, colSpan: 3, style: 'tableHeader' }, '', ''],
+              [{ text: 'E. PERIODO DE DURACIÓN DE LAS ACTIVIDADES DE PRÁCTICAS PREPROFESIONALES', colSpan: 4, style: 'tableHeader' }, '', '', ''],
+              ['TIEMPO DE DURACIÓN (Horas):', { text: '240', colSpan: 3, style: 'tableHeader' }, '', ''],
+              ['FECHA DE INICIO:\ndd/mm/aaaa', '12/03/2023', 'FECHA DE FINALIZACIÓN:\ndd/mm/aaaa', '03/05/2023'],
+            ]
+          }
+
+        },
+        {
+          text: '2. DESCRIPCIÓN DE LA EMPRESA/INSTITUCIÓN',
+          style: 'subheaderBold',
+          margin: [0, 10, 0, 0]
+        },
+        {
+          text: 'Objetivo\n\n' + this.practica.convocatoria.solicitudEmpresa.convenio.empresa.objetivo + '\n\nMisión\n\n' + this.practica.convocatoria.solicitudEmpresa.convenio.empresa.mision+ '\n\nVisión\n\n' + this.practica.convocatoria.solicitudEmpresa.convenio.empresa.vision,
+          style: 'subheader',
+          margin: [0, 10, 0, 0]
+        },
+        {
+          text: '3. DESCRIPCIÓN DETALLADA DE ACTIVIDADES REALIZADAS',
+          style: 'subheaderBold',
+          margin: [0, 10, 0, 0]
+        },
+        {
+          text: 'Objetivo\n\n' + this.practica.convocatoria.solicitudEmpresa.convenio.empresa.objetivo + '\n\nMisión\n\n' + this.practica.convocatoria.solicitudEmpresa.convenio.empresa.mision,
+          style: 'subheader',
+          margin: [0, 10, 0, 0]
+        },
+        {
+          text: '4.  EVIDENCIAS' ,
+          style: 'subheaderBold',
+          margin: [0, 10, 0, 0]
+        },
+        {
+          text: 'Lista y descripción de las evidencias presentadas\n' + '(Fotografías, capturas de pantalla, informes, diagramas, documentación, etc.)',
+          style: 'subheader',
+          margin: [0, 10, 0, 0]
+        },
+        {
+          text: '5.  CONCLUSIONES',
+          style: 'subheaderBold',
+          margin: [0, 10, 0, 0]
+        },
+        {
+          text:  this.practica.concluciones,
+          style: 'subheader',
+          margin: [0, 10, 0, 0]
+        },
+        {
+          text: '6. FIRMAS Y SELLO',
+          style: 'subheaderBold',
+          margin: [0, 10, 0, 0]
+        },
+        {
+          style: 'table',
+          table: {
+            widths: ['auto', 'auto', 'auto', 'auto'],
+            body: [
+              ['', 'ESTUDIANTE', 'TUTOR ACADÉMICO', 'TUTOR ESPECÍFICO'],
+              ['Nombre y \napellidos', { text: nomnreEst }, { text: nombreInstituto }, { text: nombreEspecifico }],
+              ['Cédula de \nIdentidad.', { text: this.practica.estudiante.usuario.cedula }, { text: this.practica.tutorInstituto.usuario.cedula }, { text: this.practica.tutorEmpresarial.usuario.cedula }],
+              ['Firmas \ny \nsellos:', '', '', ''],
+            ]
+          }
+        },
+      ],
+      styles: {
+        header: {
+          fontSize: 11,
+          bold: true,
+          margin: [0, 0, 0, 0]
+        },
+        subheader: {
+          fontSize: 11,
+          margin: [0, 5, 0, 7]
+        },
+        body: {
+          fontSize: 11,
+          margin: [0, 0, 0, 10],
+          alignment: 'justify',
+          lineHeight: 2
+        },
+        signature: {
+          alignment: 'center',
+          bold: true,
+        },
+        table: {
+          margin: [0, 10, 0, 16],
+          fontSize: 10,
+          font: 'Roboto'
+        },
+        tableHeader: {
+          bold: true
+        },
+        subheaderBold: {
+          fontSize: 11,
+          margin: [0, 5, 0, 7],
+          bold: true
+        },
+      }
+    };
+
+    // Generar el PDF
+    const pdfDocGenerator = pdfMake.createPdf(documentDefinition);
+    pdfDocGenerator.download('A8_InformeFinal.pdf');
   }
 
 
